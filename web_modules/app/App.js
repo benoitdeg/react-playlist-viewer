0dc9614eb40073dcc37536df3210fe3ec579819d
DEGUINE <benoit.deguine@sdw-104-08.local>
2016-05-11 12:05:37 +0200

    search json

--- web_modules/app/App.js
+++ web_modules/app/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Artist from 'Artist'
 import Kinds from 'Kinds'
 import List from 'List'
-import Input from 'Input'
+
 
 const artists = [
   {name:"Bob Marley"},
@@ -23,17 +23,36 @@ const kindsArray = Object.keys(kinds).map((k) => {
   return kinds[k]
 });
 
+
+
+
 export default class App extends Component {
+
+  state = {
+    artists: []
+  }
+
+  componentDidMount() {
+    fetch("https://api.spotify.com/v1/search?q=bob&type=artist").then((response) => {
+      response.json().then((data) => {
+        if (!data.error) {
+          console.log(data.artists.items)
+            this.setState({artists: data["items"]})
+        }
+
+      })
+    });
+  }
 
   render() {
     return (
       <div>
-        <div id="artists">
-            <List items={artists}/>
-        </div>
-        <div id="kinds">
-            <List items={kindsArray} />
-        </div>
+      <div id="artists">
+      <List items={artists}/>
+      </div>
+      <div id="kinds">
+      <List items={kindsArray} />
+      </div>
       </div>
     );
 
